7f5bd1bd81351704f86c73eb55ed820639b700a4
Joao Cruz <joaogsdc@gmail.com>
2020-05-28 23:37:41 -0300

    Project #3

--- Correios Einstein/src/correios/einstein/DAO/EnderecosDAO.java
+++ Correios Einstein/src/correios/einstein/DAO/EnderecosDAO.java
@@ -1,14 +1,103 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package correios.einstein.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author JoaoGSDC
  */
 public class EnderecosDAO {
-    
+
+    Connection con;
+
+    public boolean conectar() {
+        boolean retorno = false;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost/prova1?useTimezone=true&serverTimezone=UTC", "root", "admin");
+            retorno = true;
+        } catch (SQLException e) {
+            System.err.println("Erro de conexão:\n" + e);
+        }
+        return retorno;
+    }
+
+    public void salvar(int idCidade, int idBairro, int idLogradouro, String uf, String descricaoLogradouro, String descricaoCidade, String descricaoBairro, int cepLogradouro, int cepCidade) {
+        try {
+            PreparedStatement stmt;
+            stmt = (PreparedStatement) this.con.prepareStatement(
+                    // ------ UPDATE TABELA CIDADES
+                    "UPDATE cidades SET "
+                    + "descricao = '" + descricaoCidade + "', "
+                    + "cep = " + cepCidade + ", "
+                    + "uf = '" + uf + "' "
+                    + "WHERE idcidade = " + idCidade + "; "
+                    // ------ UPDATE TABELA BAIRROS
+                    + "UPDATE bairros SET "
+                    + "descricao = '" + descricaoBairro + "', "
+                    + "uf = '" + uf + "', "
+                    + "cidades_idcidade = " + idCidade
+                    + " WHERE idbairros = " + idBairro + "; "
+                    // ------ UPDATE TABELA LOGRADOUROS
+                    + "UPDATE logradouros SET "
+                    + "descricao = '" + descricaoBairro + "', "
+                    + "uf = '" + uf + "', "
+                    + "idcidade = " + idCidade + ", "
+                    + "bairros_idbairros = " + idBairro + ", "
+                    + "cep = " + cepLogradouro
+                    + " WHERE idlogradouros = " + idLogradouro + "; "
+            );
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException ex) {
+            System.err.println("Erro INSERT: " + ex);
+        }
+    }
+
+    public boolean editar(int id, String nome, String cep, String telefone, String email, String presencaWeb) {
+        boolean retorno = false;
+        try {
+            PreparedStatement stmt;
+            stmt = (PreparedStatement) this.con.prepareStatement(
+                    "UPDATE pessoas SET "
+                    + "nome = '" + nome + "', "
+                    + "cep = '" + cep + "', "
+                    + "telefone = '" + telefone + "', "
+                    + "presencaWeb = '" + presencaWeb + "' "
+                    + "WHERE id = " + id + ";");
+            stmt.execute();
+            stmt.close();
+            retorno = true;
+        } catch (SQLException ex) {
+            System.err.println("Erro UPDATE: " + ex);
+        }
+        return retorno;
+    }
+
+    public void consultar() {
+        try {
+            ResultSet rs = null;
+            PreparedStatement stmt = (PreparedStatement) this.con.prepareStatement("SELECT * FROM pessoas");
+            rs = stmt.executeQuery();
+        } catch (SQLException ex) {
+            System.err.println("Erro INSERT: " + ex);
+        }
+    }
+
+    public boolean excluir(int id) {
+        boolean retorno = false;
+        try {
+            PreparedStatement stmt;
+            stmt = (PreparedStatement) this.con.prepareStatement("DELETE FROM pessoas WHERE id = " + id);
+            stmt.execute();
+            stmt.close();
+            retorno = true;
+        } catch (SQLException ex) {
+            System.err.println("Erro DELETE: " + ex);
+        }
+        return retorno;
+    }
 }
--- Correios Einstein/src/correios/einstein/DAO/PessoasDAO.java
+++ Correios Einstein/src/correios/einstein/DAO/PessoasDAO.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package correios.einstein.DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -18,7 +14,7 @@ public class PessoasDAO {
 
     Connection con;
 
-    public boolean conecta() {
+    public boolean conectar() {
         boolean retorno = false;
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost/prova1?useTimezone=true&serverTimezone=UTC", "root", "admin");
@@ -29,26 +25,68 @@ public class PessoasDAO {
         return retorno;
     }
 
-    public void Salvar(String nome, String endereco, String bairro, String cidade, String estado, String cep, String telefone, String email, String presencaWeb) {
+    public void salvar(String nome, int cep, String telefone, String email, String presencaWeb) {
         try {
             PreparedStatement stmt;
             stmt = (PreparedStatement) this.con.prepareStatement(
                     "INSERT INTO pessoas ("
-                            + "nome, endereco, bairro, cidade, estado, cep, telefone, email, presencaWeb) "
-                            + "VALUES ("
-                            + "'" + nome + "', "
-                            + "'" + endereco + "', "
-                            + "'" + bairro + "', "
-                            + "'" + cidade + "', "
-                            + "'" + estado + "', "
-                            + "'" + cep + "', "
-                            + "'" + telefone + "', "
-                            + "'" + email + "', "
-                            + "'" + presencaWeb + "');");
+                    + "nome, cep, telefone, email, presencaWeb) "
+                    + "VALUES ("
+                    + "'" + nome + "', "
+                    + "'" + cep + "', "
+                    + "'" + telefone + "', "
+                    + "'" + email + "', "
+                    + "'" + presencaWeb + "');");
             stmt.execute();
             stmt.close();
         } catch (SQLException ex) {
             System.err.println("Erro INSERT: " + ex);
         }
+    }
+
+    public boolean editar(int id, String nome, String cep, String telefone, String email, String presencaWeb) {
+        boolean retorno = false;
+        try {
+            PreparedStatement stmt;
+            stmt = (PreparedStatement) this.con.prepareStatement(
+                    "UPDATE pessoas SET "
+                    + "nome = '" + nome + "', "
+                    + "cep = '" + cep + "', "
+                    + "telefone = '" + telefone + "', "
+                    + "presencaWeb = '" + presencaWeb + "' "
+                    + "WHERE id = " + id + ";");
+            stmt.execute();
+            stmt.close();
+            retorno = true;
+        } catch (SQLException ex) {
+            System.err.println("Erro UPDATE: " + ex);
+        }
+        return retorno;
+    }
+
+    public ResultSet consultar() {
+        try {
+            ResultSet rs = null;
+            PreparedStatement stmt = (PreparedStatement) this.con.prepareStatement("SELECT * FROM pessoas");
+            rs = stmt.executeQuery();
+            return rs;
+        } catch (SQLException ex) {
+            System.err.println("Erro INSERT: " + ex);
+        }
+        return null;
+    }
+
+    public boolean excluir(int id) {
+        boolean retorno = false;
+        try {
+            PreparedStatement stmt;
+            stmt = (PreparedStatement) this.con.prepareStatement("DELETE FROM pessoas WHERE id = " + id);
+            stmt.execute();
+            stmt.close();
+            retorno = true;
+        } catch (SQLException ex) {
+            System.err.println("Erro DELETE: " + ex);
+        }
+        return retorno;
     }
 }
